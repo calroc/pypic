5b6ae8333eac459ecefe978d1ed8b99fe5d2f08c
sajmikins <sajmikins@778b5e9c-8d31-0410-83ae-95f81496e599>
2009-05-22 01:25:27 +0000

    folded util.py into sim.py

--- trunk/sim.py
+++ trunk/sim.py
@@ -18,16 +18,96 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
     02110-1301, USA.
 '''
-from util import initialize
-from programmer import LOW_LEVEL, MID_LEVEL, Programmer
+import logging
 from myhdl import (
+    delay,
+    always,
+    now,
+    intbv,
+    bin,
     Signal,
     Simulation,
     instance,
     traceSignals,
     StopSimulation,
     )
+from programmer import LOW_LEVEL, MID_LEVEL, Programmer
 ##import parallel
+
+
+logging.basicConfig(format='%(message)s', level=10)
+log = logging.getLogger()
+
+
+_onoff = lambda n: ('off', 'on')[bool(n)]
+
+
+def _bus2int(bus):
+    res = sum(1 << n for n, line in enumerate(reversed(bus)) if line.val)
+    return res
+
+
+def ClockDriver(clock):
+    log.info('P+DS\t\t\t%s\tclock', str(now()))
+
+    @always(delay(10))
+    def driveClk():
+        next = not clock
+        log.info('\t\t\t%s\t%s', str(now()), _onoff(next))
+        clock.next = next
+
+    return driveClk
+
+
+def StrobeClockLink(clock, STROBE_BIT, strobe_enable):
+    '''Drive the chip strobe from the sim clock.'''
+
+    @instance
+    def clock_link():
+        while True:
+            yield clock.posedge
+            STROBE_BIT.next = strobe_enable and True
+            yield clock.negedge
+            STROBE_BIT.next = False
+
+    return clock_link
+
+
+def PortDriver(clock, bus):
+    prev = [-1]
+
+    @always(clock.posedge, clock.negedge)
+    def drive_port():
+        b = _bus2int(bus)
+        if b != prev[0]:
+            prev[0] = b
+            send(b)
+
+    return drive_port
+
+
+def initialize(
+    clock,
+    STROBE_BIT,
+    DATA_BIT,
+    POWER_BIT,
+    PROGRAM_BIT,
+    strobe_enable
+    ):
+    return (
+        ClockDriver(clock),
+        StrobeClockLink(clock, STROBE_BIT, strobe_enable),
+        PortDriver(
+            clock,
+            (PROGRAM_BIT, POWER_BIT, DATA_BIT, STROBE_BIT)
+            )
+        )
+
+
+def send(data):
+    '''sends data to a parallel port.'''
+    data = intbv(data)[4:]
+    log.info('%s sent\t\t%s' % (bin(data, 4), str(now())))
 
 
 def main():
